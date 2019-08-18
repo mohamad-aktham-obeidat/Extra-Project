710041ff99589ed7f2f5e31df01dd91621d66cce
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-18 17:58:41 +0300

    Extra Project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import Form from './components/Form'
 import Message from './components/Message'
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            message : ''
+        }
+    }
+    
+
+    verify = (email, name, phone, url) => {
+        console.log(this);
+        
+        if ((email && name && phone && url) === true){
+            this.setState({message: 'Form Verified'})
+        } else { 
+            this.setState({message: 'Form Not Verified'})
+        }    
+    }
 
     render() {
-        return (<div>
-            <Form></Form>
-            <Message></Message>
-        </div>);
-    }
+        return (
+        <div>
+            <Form verify={this.verify}/>
+
+            <Message msg={this.state.message}/>
+        </div>
+        )};
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {PropTypes} from 'prop-types';
 
 class Form extends Component {
     constructor(props) {
@@ -10,28 +9,47 @@ class Form extends Component {
             isPhoneValid: false,
             isUrlValid: false,
         };
-
+    }
+    validateName = (event) =>{
+        let name = event.target.value;
+        const regex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*/
+        this.setState({isNameValid: regex.test(name)});
+    }
+    validateEmail = (event) =>{
+        let email = event.target.value;
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        this.setState({isEmailValid: regex.test(email)});
+    }
+    validatePhone = (event) =>{
+        let phone = event.target.value;
+        const regex = /^2+[0-9]{10}$/;
+        this.setState({isPhoneValid: regex.test(phone)});
+    }
+    validateUrl = (event) =>{
+        let url = event.target.value;
+        const regex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+        this.setState({isUrlValid: regex.test(url)});
     }
     
     render() {
+        const {isEmailValid, isNameValid, isPhoneValid, isUrlValid} = this.state;
         return (
             <div className="row">
             <h1 className="text-center">Form Validation</h1>
             <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
+                <h5>Name:
+                </h5> <input type='text' placeholder='Enter Your Name' onChange={this.validateName}></input>
+                <h5>Email:
+                </h5> <input type='email' placeholder='Enter Your Email' onChange={this.validateEmail}></input>
+                <h5>Phone:
+                </h5> <input type='tel' placeholder='Enter Your Phone Number' onChange={this.validatePhone}></input>
+                <h5>Blog URL:
+                </h5> <input type='url' placeholder='Blog Url Here...' onChange={this.validateUrl}></input>
                 <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
+                    <a href="#" className="button success expand round text-center" onClick={this.props.verify.bind(isEmailValid, isNameValid, isPhoneValid, isUrlValid)}>Verify</a>
                 </div>
             </form>
         </div>);
     }
 }
-
 export default Form;
